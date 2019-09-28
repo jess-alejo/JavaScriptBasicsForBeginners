6897ccf8a6df7ecc1246cea69ae789a52cb9d71a
saguiguilid <tbki.jea@gmail.com>
2019-09-28 15:44:21 +0800

    3.5: Equality Operators

---
+++ section3/5.js
@@ -0,0 +1,14 @@
+// -----------------------------------
+console.log("5- Equality Operators");
+// -----------------------------------
+
+// Strict Equality: Same type and value
+console.log(1 === 1);
+console.log("1" === 1);
+console.log(true === 1);
+
+console.log("===========================");
+// Loose Equality
+console.log(1 == 1);
+console.log("1" == 1);
+console.log(true == 1);
