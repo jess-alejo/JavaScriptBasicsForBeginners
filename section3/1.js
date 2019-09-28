cb8afe474861bb629ce301d52a44a9e17a976d33
saguiguilid <tbki.jea@gmail.com>
2019-09-28 14:44:11 +0800

    3.1: JavaScript Operators

---
+++ section3/1.js
@@ -0,0 +1,10 @@
+// -----------------------------------
+// 1- JavaScript Operators
+// -----------------------------------
+
+// Operators (variables) -> Expressions -> Algorithms
+
+// Types of Operators
+let types = ["Arithmetic", "Assignment", "Comparison", "Logical", "Bitwise"];
+
+console.log("The following are types of JavaScript operators", types);
