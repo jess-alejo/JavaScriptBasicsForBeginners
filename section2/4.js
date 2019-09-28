001d519a86d2e9a8db92af88c0abb92b57d0a21a
saguiguilid <tbki.jea@gmail.com>
2019-09-28 13:23:30 +0800

    2.4: Dynamic Typing

---
+++ section2/4.js
@@ -0,0 +1,8 @@
+// -----------------------------------
+// 4. Dynamic Typing
+// -----------------------------------
+// the type of variable can change at runtime
+let item = "Banana";
+console.log("item", item, "is a", typeof item);
+item = 100;
+console.log("item", item, "is now a", typeof item);
