3fc6e1c5e0e8ef0f05121373d7f716e1b8ede92a
saguiguilid <tbki.jea@gmail.com>
2019-09-28 15:31:10 +0800

    3.4: Comparison Operators

---
+++ section3/4.js
@@ -0,0 +1,16 @@
+// -----------------------------------
+console.log("4- Comparison Operators");
+// -----------------------------------
+
+let x = 1;
+console.log("Initialize x to", x);
+
+// Relational
+console.log("Is x less than 0?", x > 0);
+console.log("Is x less than or equal to 1?", x >= 1);
+console.log("Is x less than 1", x < 1);
+console.log("Is x less than or equal to 1?", x <= 1);
+
+// Equality
+console.log("Is x equal to 1?", x === 1);
+console.log("Is x not equal to 1?", x !== 1);
