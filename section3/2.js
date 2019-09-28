9d29b2b500f9f79a59d2ea843622d1c6d0275cd9
saguiguilid <tbki.jea@gmail.com>
2019-09-28 15:06:38 +0800

    3.2: Arithmentic Operators

---
+++ section3/2.js
@@ -0,0 +1,27 @@
+// -----------------------------------
+console.log("3.2- Arithmetic Operators");
+// -----------------------------------
+
+let x = 15;
+let y = 4;
+
+console.log("x", x);
+console.log("y", y);
+
+// expression is something that produces a value
+console.log("addition", "x + y =", x + y);
+console.log("subtraction", "x - y =", x - y);
+console.log("multiplication", "x * y =", x * y);
+console.log("division", "x / y =", x / y);
+console.log("modulo", "x % y =", x % y); // remainder of division
+
+// new in ES6
+console.log("exponentiation", "x ** y =", x ** y); // x to the power of y
+
+// increment/decrement
+console.log("increment", "++x", ++x);
+// note position of ++ is important
+// ++x will increment x by 1 and perform action
+// x++ will perform action to x then increment by 1
+
+console.log("decrement", "--y", --y);
