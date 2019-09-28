49352683166ce96b79b4fe4b9270db2046a8df33
saguiguilid <tbki.jea@gmail.com>
2019-09-28 13:09:15 +0800

    2.1: Variables

---
+++ section2/1.js
@@ -0,0 +1,13 @@
+// -----------------------------------
+// 1. Variables
+// -----------------------------------
+// naming variables
+// 1. Cannot be a reserved keyword
+// 2. Should be meaningful
+// 3. Cannot start with a number (1name)
+// 4. Cannot contain a space or hyphen (-)
+// 5. Are case-sensitive
+
+let firstName = "Michael";
+let lastName = "Jordan";
+console.log(firstName, lastName);
