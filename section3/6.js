135e269dc87a510018c1adb136df1f15210404c3
saguiguilid <tbki.jea@gmail.com>
2019-09-28 15:56:05 +0800

    3.6: Ternary Operator

---
+++ section3/6.js
@@ -0,0 +1,18 @@
+// -----------------------------------
+console.log("6- Ternary Operator");
+// -----------------------------------
+
+// If a person is under 18 years old
+// that person is a minor
+// otherwise, that person is an adult
+
+let age = 17;
+let type = age < 18 ? "Minor" : "Adult";
+// age < 18
+// - a condition to evaluate, is age less than 18?
+// ? "Minor"
+// - the result if the condition is true
+// : "Adult"
+// - the result if the condition is false
+
+console.log(type);
