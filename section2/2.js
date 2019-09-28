6e72b53105d0c9e94d5920a527718afe92532a1d
saguiguilid <tbki.jea@gmail.com>
2019-09-28 13:10:22 +0800

    2.2: Constants

---
+++ section2/2.js
@@ -0,0 +1,8 @@
+// -----------------------------------
+// 2. Constants
+// -----------------------------------
+// constants are fixed
+// capital-named constants are only used as aliases for “hard-coded” values.
+
+const FAVORITE_COLOR = "blue";
+console.log("My favorite color is", FAVORITE_COLOR);
