d03b6d376624c34bee00001a851b2b5b896c44c9
saguiguilid <tbki.jea@gmail.com>
2019-09-28 13:18:15 +0800

    2.3: Primitive Types

---
+++ section2/3.js
@@ -0,0 +1,16 @@
+// -----------------------------------
+// 3. Primitive types
+// -----------------------------------
+let name = "John"; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let aliasName = undefined; // default value if not initialized
+let selectedColor = null; // explicitly clear the value of a variable
+
+console.log("The following are primitive types:");
+
+console.log(name, "is a type of", typeof name);
+console.log(age, "is a type of", typeof age);
+console.log(isApproved, "is a type of", typeof isApproved);
+console.log(aliasName, "is a type of", typeof aliasName);
+console.log(selectedColor, "is a type of", typeof selectedColor);
