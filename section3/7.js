57f2257526815bc3d42e1ac9a8e3456eac3730a6
saguiguilid <tbki.jea@gmail.com>
2019-09-28 16:09:04 +0800

    3.7: Logical Operators

---
+++ section3/7.js
@@ -0,0 +1,15 @@
+// -----------------------------------
+console.log("7- Logical Operators");
+// -----------------------------------
+
+// Logical AND (&&)
+// Returns TRUE if both operands are TRUE
+console.log(true && true);
+
+// Logical OR (||)
+// Returns TRUE if one of the operands is TRUE
+console.log(true || false);
+
+// NOT (!)
+// Opposite of the logical operator result
+console.log(!true);
