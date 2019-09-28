44826fbe007e120a1becf18b098aa6648abd1e29
saguiguilid <tbki.jea@gmail.com>
2019-09-28 15:23:11 +0800

    3.3: Assignment Operators

---
+++ section3/3.js
@@ -0,0 +1,19 @@
+// -----------------------------------
+console.log("3.3- Assignment Operators");
+// -----------------------------------
+
+// initialization
+let x = 5;
+console.log("x =", x);
+
+// increment
+x++;
+console.log("increment:", "x++", x);
+
+x = 5;
+x = x + 5;
+console.log("increase by 5:", "x = x + 5", x);
+
+x = 5;
+x += 5;
+console.log("shorthand:", "x += 5", x);
