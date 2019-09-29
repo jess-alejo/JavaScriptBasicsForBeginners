60235d386877cdcacafef2bc2b43f27572f4ee28
saguiguilid <tbki.jea@gmail.com>
2019-09-29 18:34:29 +0800

    4.11: Exercise 2- Landscape or Portrait

---
+++ section4/11.js
@@ -0,0 +1,12 @@
+// -----------------------------------
+console.log("11- Exercise 2- Landscape or Portrait");
+// -----------------------------------
+
+function isLandscape(width, height) {
+  return width > height;
+}
+
+let width = 85;
+let height = 110;
+
+console.log("Landscape?", isLandscape(width, height));
