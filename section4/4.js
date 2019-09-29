2fd916740f326aba9971a22fe453bfb9892b86e0
saguiguilid <tbki.jea@gmail.com>
2019-09-29 16:09:12 +0800

    4.4: While Loop

---
+++ section4/4.js
@@ -0,0 +1,11 @@
+// -----------------------------------
+console.log("4- While");
+// -----------------------------------
+
+let i = 1;
+
+while (i < 10) {
+  // display odd numbers
+  if (i % 2) console.log(i);
+  i++;
+}
