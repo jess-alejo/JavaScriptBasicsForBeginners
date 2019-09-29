c7a11561f5b3e9b89b6fb635058b3e4342962c54
saguiguilid <tbki.jea@gmail.com>
2019-09-29 17:50:04 +0800

    4.6: Infinite Loops

---
+++ section4/6.js
@@ -0,0 +1,23 @@
+// -----------------------------------
+console.log("6- Infinite Loops");
+// -----------------------------------
+
+let i = 0;
+// caution: for demo purpose only
+// close the browser to terminate
+
+// while (i < 5) {
+//   console.log(i);
+// }
+
+// while (true) {
+//   console.log("Will run forever");
+// }
+
+// for (let index = 0; index < 10; ) {
+//   console.log("Will run forever");
+// }
+
+// do {
+//   console.log("Will run forever");
+// } while (true);
