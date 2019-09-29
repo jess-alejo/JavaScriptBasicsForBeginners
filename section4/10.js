7c179c1cda058916e7d186b204cddec03894c028
saguiguilid <tbki.jea@gmail.com>
2019-09-29 18:28:08 +0800

    4.10: Exercise 1- Max of Two Numbers

---
+++ section4/10.js
@@ -0,0 +1,20 @@
+// -----------------------------------
+console.log("10- Exercise 1- Max of Two Numbers");
+// -----------------------------------
+
+// Write a function that takes two numbers
+// and returns the maximum of the two
+
+// using if..else
+// function max(num1, num2) {
+//   if (num1 > num2) return num1;
+
+//   return num2;
+// }
+
+// using ternary operation
+function max(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+console.log(max(3, 3));
