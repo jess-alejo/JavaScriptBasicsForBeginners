3b50913341c6f5760b9cd2232db0d305c05f2f46
saguiguilid <tbki.jea@gmail.com>
2019-09-29 10:56:25 +0800

    3.11: Exercise - Swapping Variables

---
+++ section3/11.js
@@ -0,0 +1,19 @@
+// -----------------------------------
+console.log("11- Exercise - Swapping Variables");
+// -----------------------------------
+
+let a = "red";
+let b = "blue";
+
+console.log("before swapping");
+console.log("a", a);
+console.log("b", b);
+// -----------------------------------
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("after swapping");
+console.log("a", a);
+console.log("b", b);
