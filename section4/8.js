5fc678f10adbf10318b0c02d18027eafc801824f
saguiguilid <tbki.jea@gmail.com>
2019-09-29 18:01:39 +0800

    4.8: For...of Loop

---
+++ section4/8.js
@@ -0,0 +1,11 @@
+// -----------------------------------
+console.log("8- For...of Loop");
+// -----------------------------------
+// starting ES6
+// returns the element (not index)
+
+let colors = ["red", "blue", "yellow"];
+
+for (const color of colors) {
+  console.log(color);
+}
