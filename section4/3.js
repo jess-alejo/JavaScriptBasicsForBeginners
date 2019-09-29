b423d61b4083e3c21675bf0d6729507d8668d020
saguiguilid <tbki.jea@gmail.com>
2019-09-29 16:05:16 +0800

    4.3: For Loop

---
+++ section4/3.js
@@ -0,0 +1,9 @@
+// -----------------------------------
+console.log("3- For");
+// -----------------------------------
+
+// for (initialExpression; condition; increment)
+for (let i = 1; i < 10; i++) {
+  // display odd numbers
+  if (i % 2 !== 0) console.log(i);
+}
