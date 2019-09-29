1473f732acee82f3979207ff83cc970d08104b92
saguiguilid <tbki.jea@gmail.com>
2019-09-29 17:41:15 +0800

    4.5: Do...While Loop

---
+++ section4/5.js
@@ -0,0 +1,11 @@
+// -----------------------------------
+console.log("5- Do...While Loop");
+// -----------------------------------
+
+let i = 1;
+do {
+  if (i % 2) console.log(i); // remember 0 is falsy
+  i++;
+} while (i < 10);
+
+// do..while loop will be executed at least once
