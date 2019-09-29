dca64c4f0148c58122acb6181635ef07c57f47df
saguiguilid <tbki.jea@gmail.com>
2019-09-29 10:49:49 +0800

    3.10: Operators Precedence

---
+++ section3/10.js
@@ -0,0 +1,13 @@
+// -----------------------------------
+console.log("10- Operators Precedence");
+// -----------------------------------
+
+console.log(3 + 4 * 5); // 3 + 20
+// expected output: 23
+
+// use parenthesis to group complex operations
+console.log((3 + 4) * 5); // 7 * 5
+// expected output: 35
+
+// refer to this link for more info
+// http://jsoperatorprecedence.com/
