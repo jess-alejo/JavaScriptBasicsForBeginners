b9d81b016a55497898f325fe79a0675b2673ff16
saguiguilid <tbki.jea@gmail.com>
2019-09-29 15:33:54 +0800

    4.1: If...else

---
+++ section4/1.js
@@ -0,0 +1,18 @@
+// -----------------------------------
+console.log("1- If...else");
+// -----------------------------------
+
+// Hour
+// If hour is greater than or equal 6pm: Good evening!
+// If hour is greater than or equal 12pm: Good afternoon!
+// Otherwise: Good morning
+
+let hour = 13;
+
+if (hour >= 18) {
+  console.log("Good evening!");
+} else if (hour >= 12) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good morning!");
+}
