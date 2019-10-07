f75821f1f99840047182d87ba14897a0f6f7cdff
saguiguilid <tbki.jea@gmail.com>
2019-10-07 11:03:42 +0800

    5.3: Constructor Functions

---
+++ section5/3.js
@@ -0,0 +1,12 @@
+console.log("3- Constructor Functions");
+
+// Constructor Function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw");
+  };
+}
+
+const circle = new Circle(1);
+circle1.draw();
